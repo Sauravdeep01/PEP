3601d53780191e3f4011e84cd607bfdb2da8b1b4
sauravdeep01 <sauravdeep0001@gmail.com>
2026-02-09 16:01:53 +0530

    add: studentData file

---
+++ NodeJs/StudentData/index.js
@@ -0,0 +1,89 @@
+import express from "express";
+import mongoose from "mongoose";
+import User from "./schema.js";
+
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect("mongodb+srv://saurav_01:0003%40Saurav@mycluster.tp8ebka.mongodb.net/Users")
+  .then(() => console.log("MongoDB is connected"))
+  .catch((err) => console.log(err));
+
+
+
+app.post("/users", async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
+
+app.get("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+
+app.put("/users/:id", async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+
+    if (!deletedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+
+app.listen(3000, () => {
+  console.log("Server running at port 3000");
+});
